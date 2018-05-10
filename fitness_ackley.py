f7a2fe65998fec1b4a93bb09b5938297e0299a70
chcp <chcp@cin.ufpe.br>
2018-05-10 17:53:01 -0300

    Optimized using np

--- fitness_ackley.py
+++ fitness_ackley.py
@@ -19,9 +19,10 @@ def fitness_ackley(genotype = [0.0]):
     """
     c1, c2 ,c3 = 20.0, 0.2, np.pi*2
     numGenes = np.int16(len(genotype))
-    sum1 = np.sum( [ i ** 2 for i in genotype] )
-    sum2 = np.sum( np.cos( c3 * i ) for i in genotype )
-
+    genotype = np.array(genotype)
+    sum1 = np.sum( genotype**2 )
+    sum2 = np.sum( np.cos(c3 * genotype ))
+    
     term1 = (-c1) * np.exp( -c2 * (((1 / numGenes) * sum1) ** 0.5 ))
     term2 = -np.exp(( 1 / numGenes ) * sum2)
 
